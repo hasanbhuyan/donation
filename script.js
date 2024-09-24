58dc4dd41c90499cbb8b3b4394cf183a29e56025
hasanbhuyan <muhammadhasanbhuyan.com>
2024-09-24 21:07:35 +0600

    modal complete

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.getElementById('btn-blog').addEventListener('click',function(){
     window.location.href = '/blog.html'
 })
+const date = new Date();
+const dateStr = date.toDateString()
+const currentDate = new Date(dateStr);
 
 function strToNumber(str){
     const strValue = str.innerText;
@@ -9,30 +12,26 @@ function strToNumber(str){
 }
 
 
-function donationValue(donationInput, newAmountId){
+function donationValue(donationInput, newAmountId,donationPlace){
    const previousDonationStr = document.getElementById(newAmountId);
    const totalAmountStr = document.getElementById('total-amount');
    const newDonationStr = document.getElementById(donationInput).value;
    const newDonation = parseFloat(newDonationStr)
-   
+
    const previousDonation = strToNumber(previousDonationStr)
    const totalAmount = strToNumber (totalAmountStr)
-   console.log(previousDonation,newDonation,totalAmount)
    
     if(typeof newDonation === 'number' && newDonation > 0 && totalAmount > 0 ){
+        document.getElementById('my_modal_5').showModal()
         previousDonationStr.innerText = previousDonation + newDonation;
         const currentAmount = totalAmount - newDonation;
         totalAmountStr.innerText = currentAmount;
-
-        // const div = document.createElement('div');
-        // h1.innerText = `${newDonation} Taka is Donated for  , Bangladesh `;
         const donateHistory = document.getElementById('history-section');
         const div = document.createElement('div');
         div.innerHTML =`<div class="my-10 py-10 px-3 border border-gray-200 rounded-md">
-        <h1 class="font-semibold text-xl">${newDonation} Taka is Donate For ,Bangladesh</h1>
-        <p class="font-bold">dkkdjkjdfkjfkfjkdfjdjf fjkjd kjfkjf sl sj</p>
+        <h1 class="font-semibold text-xl">${newDonation} Taka is Donate For Flood at ${donationPlace} ,Bangladesh</h1>
+        <p>Date: ${currentDate}</p>
         </div>`;
-
         donateHistory.appendChild(div)
 
         // donateHistory.appendChild(h1);
@@ -43,19 +42,20 @@ function donationValue(donationInput, newAmountId){
     }
 }
 document.getElementById('donate-noakhali').addEventListener('click',function(){
-    donationValue('input-noakhali','amount-noakhali')
+    donationValue('input-noakhali','amount-noakhali','Noakhali')
 })
 document.getElementById('donate-feni').addEventListener('click',function(){
-    donationValue('input-feni','amount-feni')
+    donationValue('input-feni','amount-feni','Feni')
 })
 document.getElementById('donate-quota-protest').addEventListener('click',function(){
-    donationValue('input-quota-protest','amount-quota-protest')
+    donationValue('input-quota-protest','amount-quota-protest','the Quota Movement')
 })
-document.getElementById('btn-donation').addEventListener('click',function(){
+const donationButton = document.getElementById('btn-donation').addEventListener('click',function(){
     document.getElementById('donation-section').classList.remove('hidden')
     document.getElementById('history-section').classList.add('hidden')   
 })
-document.getElementById('btn-history').addEventListener('click',function(){
+const historyButton =  document.getElementById('btn-history').addEventListener('click',function(){
     document.getElementById('history-section').classList.remove('hidden')
     document.getElementById('donation-section').classList.add('hidden')
+    
 })
